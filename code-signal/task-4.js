f4cd3c1673c86dca90bdf079ed84c6eaa8bea110
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-10-26 12:52:21 +0300

    CodeSignal task-4 was resolved.

---
+++ code-signal/task-4.js
@@ -0,0 +1,11 @@
+//Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+const fn = arr => {
+  let sum = arr[0] * arr[1];
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] * arr[i + 1] > sum) {
+      sum = arr[i] * arr[i + 1];
+    }
+  }
+  return sum;
+}
