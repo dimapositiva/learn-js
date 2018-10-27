c0c4010dafa5651eb95023f01947313a54b7bef1
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-10-27 09:25:08 +0300

    Task 1 (chapter 6) was resolved.

---
+++ js-for-kids/ch-06/name-checker.js
@@ -0,0 +1,11 @@
+const nameChecker = name => {
+  if (name === 'John') {
+    console.log('Hello John!');
+  } else if (name === 'Mark') {
+    console.log('Hello dad!');
+  } else if (name === 'Elizabeth') {
+    console.log('Hello mom!');
+  } else {
+    console.log('Hello stranger!');
+  };
+}
