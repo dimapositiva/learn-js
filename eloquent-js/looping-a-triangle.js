1f064cf4d3d66c97e56e5442915e46dcacf53f3e
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-11-10 15:56:29 +0200

    Task 1 - 'Looping a triangle' (chapter 2) was resolve.

---
+++ eloquent-js/looping-a-triangle.js
@@ -0,0 +1,16 @@
+/* Write a loop that makes seven calls to console.log to output the following
+triangle */
+
+let result = '';
+
+for (let i = 0; i < 7; i++) {
+  result += '#';
+  console.log(result);
+}
+
+//other decision wit while lopp
+let res = '';
+while (res.length < 7) {
+  res += '#';
+  console.log(res);
+}
