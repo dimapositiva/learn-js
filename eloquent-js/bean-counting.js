f5d6c167361eda95b00d4e40d03c5b9e54b6f6af
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-11-14 19:12:33 +0200

    Task 3 'Bean counting' (chapter 3) was resolve.

---
+++ eloquent-js/bean-counting.js
@@ -0,0 +1,29 @@
+/*
+Write a function countBs that takes a string as its only argument and returns
+a number that indicates how many uppercase “B” characters there are in the
+string.
+Next, write a function called countChar that behaves like countBs, except
+it takes a second argument that indicates the character that is to be counted
+(rather than counting only uppercase “B” characters). Rewrite countBs to
+make use of this new function.
+*/
+
+const countBs = string => {
+  let b = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === 'B') {
+      b++;
+    }
+  }
+  return b;
+}
+
+const countChar = (string, char) => {
+  let b = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      b++;
+    }
+  }
+  return b;
+}
