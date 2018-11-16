5c0f4d77f2d60bb7e86e3a47f69a1a081d358622
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-11-16 14:12:33 +0200

    Code refactoring

--- code-signal/task-4.js
+++ code-signal/task-4.js
@@ -1,11 +1,12 @@
-//Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+/*  Given an array of integers, find the pair of adjacent elements that has
+the largest product and return that product.  */
 
-const fn = arr => {
+const fn = (arr) => {
   let sum = arr[0] * arr[1];
-  for (i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] * arr[i + 1] > sum) {
       sum = arr[i] * arr[i + 1];
     }
   }
   return sum;
-}
+};
