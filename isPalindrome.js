19d6114de21f6e112b93df03acf3f96e2a3aafdf
Soloviov Dmytro <d.v.solovyov1@gmail.com>
2019-03-18 11:59:54 +0200

    Adding alternative solution

--- isPalindrome.js
+++ isPalindrome.js
@@ -7,3 +7,20 @@ const isPalindrome = n => {
     return false;
   }
 }
+
+//alternative solution
+const isPalindrome2 = n => {
+  let y = n;
+  let z = 0;
+  let x = 0;
+  while (y > 0) {
+    x = y % 10;
+    z = z * 10 + x;
+    y = Math.floor(y / 10);
+  }
+  if (n === z) {
+    return true;
+  } else {
+    return false;
+  }
+}
